3012acd5e817dfb6c7e74debe496be7d9a270b78
Parth482 <parth_22002@ldrp.ac.in>
2026-04-07 07:24:37 +0530

    refactor: update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  turbopack: {
-    root: '/Users/madushankafernando/Downloads/b_hc5DhIcpvVt-1775169776011',
-  },
+ 
 }
 
 export default nextConfig
