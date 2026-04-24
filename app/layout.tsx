caefa2a716af8868ca84568209777b38f70dc7ca
Parth482 <parth_22002@ldrp.ac.in>
2026-04-24 08:11:44 +0530

    feat: Update favicon to fixes project icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,16 +18,15 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/fav-icon.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/fav-icon.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
+        url: '/fav-icon.png',
       },
     ],
     apple: '/apple-icon.png',
